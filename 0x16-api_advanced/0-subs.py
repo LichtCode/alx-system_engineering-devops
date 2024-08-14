4c6bf2cccbd6005234ef51cb8a1d53f3ccd31acf
LichtCode <prinzdarey#gmail.com>
2024-08-13 20:06:52 -0700

    made more changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+    this sends a query to Reddit API
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """Makes a request to Reddit"""
+
+    if subreddit is None:
         return 0
+    elif isinstance(subreddit, str):
+        headers = {"User-Agent": "Mozilla/5.0"}
+        url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+        req = requests.get(url, headers=headers, allow_redirects=False)
+        if req.status_code == 200:
+            response = req.json()
+            try:
+                return response.get('data').get('subscribers')
+            except (AttributeError, TypeError):
+                return 0
